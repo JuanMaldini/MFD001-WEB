207f01a948f278451afcf6793b9ab6aad16d8ad5
Juan Maldini <juan.maldini@outlook.com>
2026-02-05 10:44:25 -0300

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ export default function Home() {
         <main className="bg-black gap-2 flex flex-col min-h-screen items-center justify-center">
 
             <div className='flex flex-wrap gap-2'>
-                <Link href="/E3DS">
+                <Link href="/design-planner">
                     <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
                         Start app
                     </button>
                 </Link>
 
-                <Link href="/send-email">
+                {/* <Link href="/send-email">
                     <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
                         Send email
                     </button>
-                </Link>
+                </Link> */}
             </div>
 
-            <div className='flex flex-wrap gap-2'>
+            {/* <div className='flex flex-wrap gap-2'>
                 <Link href="/partition-operable">
                     <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
                         Operable Partition
@@ -40,7 +40,7 @@ export default function Home() {
                         Accordion Partition
                     </button>
                 </Link>
-            </div>
+            </div> */}
 
         </main>
     );
